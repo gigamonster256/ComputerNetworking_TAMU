e32569da277b0ce66c743c906d43a2a4223b312b
Caleb Norton <n0603919@outlook.com>
2024-08-19 19:18:02 -0500

    Nix init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "ECEN 602: Computer Networking";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    pkgsFor = lib.genAttrs systems (
+      system: import nixpkgs {inherit system;}
+    );
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    MPs = forEachSystem (pkgs: import ./MPs.nix {inherit pkgs;});
+  in {
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+    packages = MPs;
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+  };
+}
