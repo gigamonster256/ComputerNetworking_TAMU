8c20a96192e6349d9bc07819cc4b2e1667ad008f
Caleb Norton <n0603919@outlook.com>
2024-10-23 15:24:04 -0500

    Add ns-2 simulator

--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    personal-packages = {
+      url = "github:gigamonster256/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
+    personal-packages,
     ...
   }: let
     inherit (nixpkgs) lib;
@@ -17,7 +23,7 @@
       "aarch64-darwin"
     ];
     pkgsFor = lib.genAttrs systems (
-      system: import nixpkgs {inherit system;}
+      system: import nixpkgs {inherit system;} // personal-packages.packages.${system}
     );
     forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
     MPs = forEachSystem (pkgs: import ./MPs.nix {inherit pkgs;});
--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@
   default = pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       cloc
+      ns-2 # Network Simulator 2 from personal nur-packages
     ];
   };
 }
