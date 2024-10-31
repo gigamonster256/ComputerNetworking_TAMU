5f8e68662d7cf9ea38f4b8281f448ba85a87ef68
Caleb Norton <n0603919@outlook.com>
2024-10-31 13:43:34 -0500

    Add placeholder zip script

---
+++ MP4/zip_submission.sh
@@ -0,0 +1,21 @@
+team_id=9
+
+MP=4
+
+# clean all files
+make -C .. clean
+
+mkdir -p mp$MP\_$team_id
+rm -rf mp$MP\_$team_id/*
+
+LIBS="../libtcp ../libhttp"
+
+cp -r src/* $LIBS mp$MP\_$team_id/
+
+# go through src/makefile and change ../../ to nothing
+sed -i 's/..\/..\/libtcp/libtcp/g' mp$MP\_$team_id/makefile
+sed -i 's/..\/..\/libhttp/libhttp/g' mp$MP\_$team_id/makefile
+
+zip -r mp$MP\_$team_id.zip mp$MP\_$team_id
+
+rm -rf mp$MP\_$team_id
